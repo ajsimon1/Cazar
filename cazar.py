3cf6d54469c29f4b11cef8b253545068a6e8bd6d
Adam Simon <asimon@billtrust.local>
2018-10-17 14:53:04 -0400

    added methods to XmlClass

--- cazar.py
+++ cazar.py
@@ -4,7 +4,12 @@ Created on Tue Sep 25 16:25:46 2018
 
 @author: asimon
 """
+# import os
 import re
+import xml.etree.ElementTree as et
+
+# os.getcwd().split('\\')[0:4]
+# '\\'.join(os.getcwd().split('\\')[0:4])
 
 class PrintFile(object):
     '''
@@ -65,7 +70,7 @@ class PrintFile(object):
             split_doc = doc[0].split('\n')
             # updating numbers provided to counter zero based index
             # sets provided line, strt, end #s as key of dict for each doc
-            doc_dict[split_doc[doc_num_line - 1][doc_num_strt - 1:doc_num_end - 1]] = split_doc
+            doc_dict[str(split_doc[doc_num_line - 1][doc_num_strt - 1:doc_num_end - 1]).strip()] = split_doc
         self.parsed_dict = doc_dict
         return doc_dict
     
@@ -76,4 +81,118 @@ class PrintFile(object):
             print('{0}: {1}'.format(k, v[int(line_num)-1][int(strt_pos)-1:int(end_pos)-1]))
     
     # TODO find a way to check for multi pages and accomodate
+    
+    # TODO create base class with standard methods, specific file classes then 
+    # inherit off the base 
+    
+class XmlFile(object):
+    def __init__(self, file_path, doc_keyword):
+        # doc_keyword param identifies element that marks separation of docs
+        # within the xml
+        self.file_path = file_path
+        self.doc_keyword = doc_keyword
+        # parse xml
+        self.tree = et.parse(file_path)
+        # get root node of document
+        self.root = self.tree.getroot()
+        # return all sub elements
+        self.elements_list = [i.tag for i in self.root.findall('.//')]
+        
+    def get_doc_by_num(self, doc_num):
+        a = self.root.findall('.//{}'.format(self.doc_keyword))
+        for i in a:
+            if i[2][0].text.strip() == doc_num:
+                return i
+                break
+            else:
+                pass
+        print('Document #: {} was not found'.format(doc_num))
+        return
+    
+    def get_all_docs(self):
+        return self.root.findall('.//{}'.format(self.doc_keyword))
+    
+    def prnt_all_for_elem(self, elem):
+        return [i.text for i in self.root.findall('.//{}'.format(elem))]
+    
+    def get_root(self):
+        return self.root
+    
+    def get_tree(self):
+        return self.tree
+    
+    def check_all_elem_agnst_value(self, elem, val):
+        a_list = []
+        for i in self.get_all_docs():
+            try:
+                if i.find('.//{}'.format(elem)).text.strip() == '{}'.format(val):
+                    a_list.append(i[2][0].text.strip())
+            except AttributeError:
+                pass
+        return a_list
+    
+    def get_max_val(self, elem):
+        elem_list = []
+        for i in self.get_all_docs():
+            for a in i.findall('.//{}'.format(elem)):
+                try:
+                    elem_list.append(a.text.strip())
+                except AttributeError:
+                    pass
+        return max(elem_list, key=len, default=None)
+    
+    def build_parent_map(self):
+        # change s[0] to stmts
+        parent_map = dict((c, p) for p in s[0].iter() for c in p)
+        # this is going to miss parent/children relationships that are not
+        # terminal, ie parent/children that are both nested nodes
+            
+            
+        
+            
+                 
+    
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
